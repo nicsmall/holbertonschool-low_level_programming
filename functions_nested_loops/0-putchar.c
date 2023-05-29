dd6baec2b9d6665a968fb86efd8846b068831e58
nicsmall <nicsmall50@gmail.com>
2023-05-29 12:05:23 -0700

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 c
-#include <main.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -8,14 +8,6 @@ c
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	putchar('\n');
 	return (0);
 }
